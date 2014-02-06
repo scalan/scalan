fd81373ddeee30801eb9d463a174267ebe9dee83
Alexey Romanov <alexey.v.romanov@huawei.com>
2014-02-06 11:45:22 +0400

    More usable IF/THEN/ELSE/ELSEIF

--- src/main/scala/scalan/primitives/IfThenElse.scala
+++ src/main/scala/scalan/primitives/IfThenElse.scala
@@ -6,17 +6,34 @@ import scalan.{ScalanStaged, ScalanSeq, Base, Scalan}
 trait IfThenElse extends Base { self: Scalan =>
   def __ifThenElse[T](cond: Rep[Boolean], thenp: => Rep[T], elsep: => Rep[T]): Rep[T]
 
-  def IF[T](cond: Rep[Boolean])(thenp: => Rep[T]) = new {
+  def IF(cond: Rep[Boolean]): IfBranch = new IfBranch(cond)
+  
+  class IfBranch(cond: Rep[Boolean]) {
+    def apply[T](thenp: => Rep[T]) = THEN(thenp)
+    
+    def THEN[T](thenp: => Rep[T]) = new ThenBranch[T](cond, thenp)
+  }
+  
+  class ElseIfBranch[T](cond: Rep[Boolean], outer: ThenBranch[T]) {
+    def apply(thenp: => Rep[T]) = THEN(thenp)
+    
+    def THEN(thenp: => Rep[T]) = new ThenBranch[T](cond, thenp) {
+      override def ELSE(elsep: => Rep[T]) = outer.elseIf(cond, thenp, elsep)
+    }
+  }
+  
+  class ThenBranch[T](cond: Rep[Boolean], thenp: => Rep[T]) {
     def ELSE(elsep: => Rep[T]): Rep[T] = __ifThenElse(cond, thenp, elsep)
+    
+    def elseIf(cond1: => Rep[Boolean], thenp1: => Rep[T], elsep1: => Rep[T]) = 
+      ELSE(__ifThenElse(cond1, thenp1, elsep1))
+    
+    def ELSEIF(cond1: => Rep[Boolean]) = new ElseIfBranch[T](cond1, this)
   }
 }
 
-object IfThenElseHack {
-  def ifThenElse[T](cond: Boolean, thenp: => T, elsep: => T): T = if (cond) thenp else elsep
-}
-
 trait IfThenElseSeq extends IfThenElse { self: ScalanSeq =>
-  def __ifThenElse[T](cond: Rep[Boolean], thenp: => Rep[T], elsep: => Rep[T]): Rep[T] = IfThenElseHack.ifThenElse(cond, thenp, elsep)
+  def __ifThenElse[T](cond: Rep[Boolean], thenp: => Rep[T], elsep: => Rep[T]): Rep[T] = if(cond) thenp else elsep
 }
 
 trait IfThenElseExp extends IfThenElse with BaseExp { self: ScalanStaged =>
@@ -36,5 +53,3 @@ trait IfThenElseExp extends IfThenElse with BaseExp { self: ScalanStaged =>
     case _ => super.rewrite(d)
   }
 }
-
-
