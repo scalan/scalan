2c02cf247c730d8ee533966691cb9c4d273fdc77
Alexey Romanov <alexey.v.romanov@huawei.com>
2014-02-13 17:04:25 +0400

    Fixed test filtering

--- project/Build.scala
+++ project/Build.scala
@@ -23,39 +23,36 @@ object ScalanLiteBuild extends Build {
     "ch.qos.logback" % "logback-classic" % "1.0.13",
     "com.chuusai" % "shapeless_2.10.3" % "2.0.0-M1")
 
-  // TODO there is some problem with filtering
   val testSettings = Seq(
     testOptions in Test := Seq(Tests.Filter(x => !itFilter(x))),
     testOptions in ItTest := Seq(Tests.Filter(x => itFilter(x))),
     testFrameworks in PerfTest := Seq(new TestFramework("org.scalameter.ScalaMeterFramework")),
     logBuffered in PerfTest := false,
-    // testOptions in PerfTest := Seq(Tests.Filter(perfFilter)),
     // needed thanks to http://stackoverflow.com/questions/7898273/how-to-get-logging-working-in-scala-unit-tests-with-testng-slf4s-and-logback
     parallelExecution in Test := false,
     parallelExecution in ItTest := false,
     parallelExecution in PerfTest := false)
 
-  val commonSettings = Seq(
-    // scalaVersion := "2.10.3",
-    organization := "com.huawei",
-    version := "0.1-SNAPSHOT",
-    opts, commonDeps) ++ testSettings
+  val commonSettings = inConfig(ItTest)(Defaults.testTasks) ++ 
+    inConfig(PerfTest)(Defaults.testTasks) ++ Seq(
+      scalaVersion := "2.10.3",
+      organization := "com.huawei",
+      version := "0.1-SNAPSHOT",
+      opts, commonDeps) ++ 
+    testSettings
 
   lazy val ItTest = config("it").extend(Test)
 
   lazy val PerfTest = config("perf").extend(Test)
 
-  def perfFilter(name: String) = name.startsWith("tests.performance")
+  def itFilter(name: String): Boolean = name.contains("ItTests")
 
-  def itFilter(name: String): Boolean = false // name.contains("ItTests")
-
-  lazy val core = Project("scalan-lite", file("core")).configs(ItTest, PerfTest).settings(commonSettings: _*)
+  lazy val core = Project("scalan-lite", file("core")).configs(ItTest, PerfTest).
+    settings(commonSettings: _*)
   
   lazy val lmsBackend = Project("lms-backend", file("lms-backend")).dependsOn(core % "compile->compile;test->test").configs(ItTest, PerfTest).
     settings(commonSettings: _*).settings(libraryDependencies += "EPFL" % "lms_2.10" % "0.3-SNAPSHOT")
   
   // name to make this the default project
-  lazy val root = Project("all", file(".")).aggregate(core, lmsBackend).settings(commonSettings: _*)
-
-  scalaVersion in Global := "2.10.3"
+  lazy val root = Project("all", file(".")).aggregate(core, lmsBackend).configs(ItTest, PerfTest).settings(commonSettings: _*)
 }
