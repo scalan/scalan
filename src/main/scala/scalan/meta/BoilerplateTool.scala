aa3264bed97ffd5045a26544b56e0e714c328d22
Alexey Romanov <alexey.v.romanov@huawei.com>
2014-02-26 12:16:09 +0400

    Fix paths, use Scalan for generation

--- src/main/scala/scalan/meta/BoilerplateTool.scala
+++ src/main/scala/scalan/meta/BoilerplateTool.scala
@@ -8,10 +8,11 @@ object BoilerplateTool extends App {
   val defConf = CodegenConfig.default
 
   val scalanConfig = defConf.copy(
-    srcPath = "/home/s00747473/Projects/scalan/src",
+    srcPath = "../scalan/src",
     entityFiles = List(
-      "main/scala/scalan/trees/Trees.scala"
-      , "main/scala/scalan/math/Matrices.scala"
+      "main/scala/scalan/trees/Trees.scala",
+      "main/scala/scalan/math/Matrices.scala",
+      "main/scala/scalan/collections/Sets.scala"
     ),
     emitSourceContext = true,
     isoNames = ("A","B")
@@ -26,6 +27,6 @@ object BoilerplateTool extends App {
     stagedViewsTrait = "ViewsExp"
   )
 
-  val ctx = new EntityManagement(liteConfig)
+  val ctx = new EntityManagement(scalanConfig)
   ctx.generateAll
 }
