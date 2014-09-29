3fc1eeb2aea28b8eff2cb17f4a0338295e7e3fec
Alexey Romanov <alexey.romanov@huawei.com>
2014-09-29 11:02:15 +0400

    Extracted ItTestsUtil trait

--- core/src/test/scala/scalan/it/BaseItTests.scala
+++ core/src/test/scala/scalan/it/BaseItTests.scala
@@ -1,46 +1,5 @@
 package scalan.it
 
-import java.io.File
-
-import scala.io.Source
 import scalan.BaseTests
-import scalan.codegen.Backend
 
-abstract class BaseItTests extends BaseTests {
-
-  lazy val prefix: String =
-    getClass.getSimpleName.stripSuffix("Tests").stripSuffix("It").stripSuffix("_")
-
-  def emitGraphs: Boolean = true
-
-  def readFile(name: String): String =
-    Source.fromFile(name).getLines().toIterator.mkString("\n")
-
-  def assertFileContentCheck(name: String): Unit = {
-    readFile(name + ".check") should be(readFile(name))
-  }
-
-  def getStagedOutput[A, B](back: Backend)(f: back.Exp[A => B], functionName: String, input: A): B = {
-    val dir = new File(new File("it-out", prefix), functionName)
-
-    back.buildExecutable(dir, functionName, f, emitGraphs)
-    back.execute(dir, functionName, input, f)
-  }
-
-  implicit def defaultComparator[A](expected: A, actual: A) {
-    actual should equal(expected)
-  }
-
-  def compareOutputWithSequential[A, B](back: Backend)
-                                       (fSeq: A => B, f: back.Exp[A => B], functionName: String, input: A)
-                                       (implicit comparator: (B, B) => Unit) {
-    compareOutputWithExpected(back)(fSeq(input), f, functionName, input)
-  }
-
-  def compareOutputWithExpected[A, B](back: Backend)
-                                     (expected: B, f: back.Exp[A => B], functionName: String, input: A)
-                                     (implicit comparator: (B, B) => Unit) {
-    val actual = getStagedOutput(back)(f, functionName, input)
-    comparator(expected, actual)
-  }
-}
+abstract class BaseItTests extends BaseTests with ItTestsUtil
---
+++ core/src/test/scala/scalan/it/ItTestsUtil.scala
@@ -0,0 +1,48 @@
+package scalan.it
+
+import java.io.File
+
+import org.scalatest.{Matchers, Suite}
+
+import scala.io.Source
+import scalan.codegen.Backend
+
+// extracted so it can be used with different suite styles
+trait ItTestsUtil { self: Suite with Matchers =>
+
+  lazy val prefix: String =
+    getClass.getSimpleName.stripSuffix("Tests").stripSuffix("It").stripSuffix("_")
+
+  def emitGraphs: Boolean = true
+
+  def readFile(name: String): String =
+    Source.fromFile(name).getLines().toIterator.mkString("\n")
+
+  def assertFileContentCheck(name: String): Unit = {
+    readFile(name + ".check") should be(readFile(name))
+  }
+
+  def getStagedOutput[A, B](back: Backend)(f: back.Exp[A => B], functionName: String, input: A): B = {
+    val dir = new File(new File("it-out", prefix), functionName)
+
+    back.buildExecutable(dir, functionName, f, emitGraphs)
+    back.execute(dir, functionName, input, f)
+  }
+
+  implicit def defaultComparator[A](expected: A, actual: A) {
+    actual should equal(expected)
+  }
+
+  def compareOutputWithSequential[A, B](back: Backend)
+                                       (fSeq: A => B, f: back.Exp[A => B], functionName: String, input: A)
+                                       (implicit comparator: (B, B) => Unit) {
+    compareOutputWithExpected(back)(fSeq(input), f, functionName, input)
+  }
+
+  def compareOutputWithExpected[A, B](back: Backend)
+                                     (expected: B, f: back.Exp[A => B], functionName: String, input: A)
+                                     (implicit comparator: (B, B) => Unit) {
+    val actual = getStagedOutput(back)(f, functionName, input)
+    comparator(expected, actual)
+  }
+}
