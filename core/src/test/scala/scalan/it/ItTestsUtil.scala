bd17d054fcc3856eb8bbcfee61f567ee38df1794
Alexey Romanov <alexey.romanov@huawei.com>
2014-10-06 09:42:48 +0400

    Added methods taking config to ItTestUtil

--- core/src/test/scala/scalan/it/ItTestsUtil.scala
+++ core/src/test/scala/scalan/it/ItTestsUtil.scala
@@ -18,12 +18,16 @@ trait ItTestsUtil { self: Suite with Matchers =>
   def assertFileContentCheck(name: String): Unit =
     FileUtil.read(new File(prefix, name)) should be(FileUtil.read(new File(prefix, name + ".check")))
 
-  // TODO add overloads which take config (tried to add default arguments, there are some strange errors)
-  def getStagedOutput[A, B](back: Backend)(f: back.Exp[A => B], functionName: String, input: A): B = {
+  // there are bad interactions between path-dependent types and default parameters, so
+  // we can't simply make config a default parameter
+  def getStagedOutput[A, B](back: Backend)(f: back.Exp[A => B], functionName: String, input: A): B =
+    getStagedOutputConfig(back)(f, functionName, input, back.defaultConfig)
+
+  def getStagedOutputConfig[A, B](back: Backend)(f: back.Exp[A => B], functionName: String, input: A, config: back.Config): B = {
     val dir = new File(new File("it-out", prefix), functionName)
 
-    back.buildExecutable(dir, functionName, f, emitGraphs)
-    back.execute(dir, functionName, input, f)
+    back.buildExecutable(dir, functionName, f, emitGraphs)(config)
+    back.execute(dir, functionName, input, f)(config)
   }
 
   implicit def defaultComparator[A](expected: A, actual: A) {
@@ -33,13 +37,25 @@ trait ItTestsUtil { self: Suite with Matchers =>
   def compareOutputWithSequential[A, B](back: Backend)
                                        (fSeq: A => B, f: back.Exp[A => B], functionName: String, input: A)
                                        (implicit comparator: (B, B) => Unit) {
-    compareOutputWithExpected(back)(fSeq(input), f, functionName, input)
+    compareOutputWithSequentialConfig(back)(fSeq, f, functionName, input, back.defaultConfig)
+  }
+
+  def compareOutputWithSequentialConfig[A, B](back: Backend)
+                                       (fSeq: A => B, f: back.Exp[A => B], functionName: String, input: A, config: back.Config)
+                                       (implicit comparator: (B, B) => Unit) {
+    compareOutputWithExpectedConfig(back)(fSeq(input), f, functionName, input, config)
   }
 
   def compareOutputWithExpected[A, B](back: Backend)
                                      (expected: B, f: back.Exp[A => B], functionName: String, input: A)
                                      (implicit comparator: (B, B) => Unit) {
-    val actual = getStagedOutput(back)(f, functionName, input)
+    compareOutputWithExpectedConfig(back)(expected, f, functionName, input, back.defaultConfig)
+  }
+
+  def compareOutputWithExpectedConfig[A, B](back: Backend)
+                                     (expected: B, f: back.Exp[A => B], functionName: String, input: A, config: back.Config)
+                                     (implicit comparator: (B, B) => Unit) {
+    val actual = getStagedOutputConfig(back)(f, functionName, input, config)
     comparator(expected, actual)
   }
 }
