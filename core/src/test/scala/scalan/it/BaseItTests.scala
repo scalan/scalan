68460b304cbef023014f0c05a0d2ab8e03362dfa
Adel Chepkunov <achepkunov@yandex.ru>
2014-10-22 09:13:35 +0400

    add default folder name for tests

--- core/src/test/scala/scalan/it/BaseItTests.scala
+++ core/src/test/scala/scalan/it/BaseItTests.scala
@@ -2,4 +2,24 @@ package scalan.it
 
 import scalan.BaseTests
 
-abstract class BaseItTests extends BaseTests with ItTestsUtil
+abstract class BaseItTests extends BaseTests with ItTestsUtil {
+
+  var currentTestName = ""
+
+  override def withFixture(test: NoArgTest) = {
+    //todo lock currentTestName
+    currentTestName = test.name
+    super.withFixture(test)
+  }
+
+  def compareOutputWithSequential[A, B](back : scalan.compilation.Compiler)
+                                       (fSeq : scala.Function1[A, B], f : back.Exp[A => B], input : A)
+                                       (implicit comparator : (B, B) => Unit) =
+  {
+    val testName = currentTestName
+    //todo unlock currentTestName
+    super.compareOutputWithSequential(back)(fSeq, f, testName, input)
+  }
+
+
+}
