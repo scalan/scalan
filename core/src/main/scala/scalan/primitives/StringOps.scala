9e455c7c4c4f4808279cec60a9cb4466a4d416be
Alexander Slesarenko <alexander.slesarenko@huawei.com>
2015-01-14 12:50:38 +0300

    fix StringOps declarations

--- core/src/main/scala/scalan/primitives/StringOps.scala
+++ core/src/main/scala/scalan/primitives/StringOps.scala
@@ -4,18 +4,18 @@ import scalan.{  Scalan, ScalanSeq }
 
 trait StringOps extends UnBinOps { self: Scalan =>
   implicit class StringOpsCls(lhs: Rep[String]) {
-    def +(rhs: Rep[String]) = StringConcat().apply(lhs, rhs)
-    def startsWith(rhs: Rep[String]) = StringStartsWith().apply(lhs, rhs)
-    def endsWith(rhs: Rep[String]) = StringEndsWith().apply(lhs, rhs)
-    def contains(rhs: Rep[String]) = StringContains().apply(lhs, rhs)
-    def matches(rhs: Rep[String]) = StringMatches().apply(lhs, rhs)
+    def +(rhs: Rep[String]) = StringConcat(lhs, rhs)
+    def startsWith(rhs: Rep[String]) = StringStartsWith(lhs, rhs)
+    def endsWith(rhs: Rep[String]) = StringEndsWith(lhs, rhs)
+    def contains(rhs: Rep[String]) = StringContains(lhs, rhs)
+    def matches(rhs: Rep[String]) = StringMatches(lhs, rhs)
   }
 
 
-  case class StringConcat extends EndoBinOp[String]("+", _ + _)
-  case class StringContains extends BinOp[String, Boolean]("contains", _.contains(_))
-  case class StringStartsWith extends BinOp[String, Boolean]("startsWith", _.startsWith(_))
-  case class StringEndsWith extends BinOp[String, Boolean]("endsWith", _.endsWith(_))
-  case class StringMatches extends BinOp[String, Boolean]("matches", _.matches(_))
+  val StringConcat = new EndoBinOp[String]("+", _ + _)
+  val StringContains = new BinOp[String, Boolean]("contains", _.contains(_))
+  val StringStartsWith = new BinOp[String, Boolean]("startsWith", _.startsWith(_))
+  val StringEndsWith = new BinOp[String, Boolean]("endsWith", _.endsWith(_))
+  val StringMatches = new BinOp[String, Boolean]("matches", _.matches(_))
 }
 
