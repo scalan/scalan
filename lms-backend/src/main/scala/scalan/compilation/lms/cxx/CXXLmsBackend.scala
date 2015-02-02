3a77eee1e56c0d4ca68e30e279e9de563e1aa91e
Zotov Yuriy <zotov.yuriy@huawei.com>
2015-02-02 18:09:17 +0300

    fixes

--- lms-backend/src/main/scala/scalan/compilation/lms/cxx/CXXLmsBackend.scala
+++ lms-backend/src/main/scala/scalan/compilation/lms/cxx/CXXLmsBackend.scala
@@ -38,7 +38,7 @@ class CoreCXXLmsBackend extends CoreLmsBackendBase { self =>
             "*******************************************/")
         emitFileHeader()
 
-        val indargs = (0 until args.length).asInstanceOf[Range].zip(args);
+        val indargs = scala.Range(0, args.length).zip(args);
         stream.println(s"${sA} apply(${indargs.map( p => s"${remap(p._2.tp)} ${quote(p._2)}").mkString(", ")} ) {")
 
         emitBlock(body)
