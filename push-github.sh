bad5993260a01a2c32e0b331476044ea2165f2e4
Alexey Romanov <alexey.romanov@huawei.com>
2015-02-05 16:40:05 +0300

    Updated push-github.sh

--- push-github.sh
+++ push-github.sh
@@ -2,7 +2,5 @@
 set -o errexit
 
 git fetch origin
-git checkout master-github
-git pull
-git push https://github.com/scalan/scalan-ce.git origin/master-github:master
-git checkout master
+git fetch github
+git push https://github.com/scalan/scalan-ce.git origin/master:master
