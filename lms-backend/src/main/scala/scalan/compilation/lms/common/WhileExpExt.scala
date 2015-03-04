ceff1d1ac556b2a768df375c6eca61e43f03999e
Zotov Yuriy <zotov.yuriy@huawei.com>
2015-03-04 15:36:45 +0300

    FunctionsExpExt with mirrorDef for Lambda

--- lms-backend/src/main/scala/scalan/compilation/lms/common/WhileExpExt.scala
+++ lms-backend/src/main/scala/scalan/compilation/lms/common/WhileExpExt.scala
@@ -1,7 +1,7 @@
 package scalan.compilation.lms.common
 
 import scala.reflect.SourceContext
-import scala.virtualization.lms.common.{ListOpsExp, WhileExp, EffectExp, While}
+import scala.virtualization.lms.common._
 import scalan.compilation.lms.LmsBackendFacade
 
 trait WhileExpExt extends WhileExp { self: LmsBackendFacade =>
@@ -22,6 +22,17 @@ trait ListOpsExpExt extends ListOpsExp { self: LmsBackendFacade =>
         ListPrepend(f(xs),f(a)).asInstanceOf[Def[A]]
       case ListCons(a,xs) =>
         ListCons(f(a),f(xs)).asInstanceOf[Def[A]]
+      case _ =>
+        super.mirrorDef(e,f)
+    }
+  }
+}
+
+trait FunctionsExpExt extends FunctionsExp { self: LmsBackendFacade =>
+  override def mirrorDef[A:Manifest](e: Def[A], f: Transformer)(implicit pos: SourceContext): Def[A] = {
+    e match {
+      case Lambda(fu, x, y) =>
+        Lambda(f(fu),f(x),f(y)).asInstanceOf[Def[A]]
       case _ =>
         super.mirrorDef(e,f)
     }
