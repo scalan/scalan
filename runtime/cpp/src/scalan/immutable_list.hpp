7f73e32d3b6292f27a76f4b86e274f8bbf41d3cc
Zotov Yuriy <zotov.yuriy@huawei.com>
2015-03-16 10:39:58 +0300

    immutable_list.hpp

---
+++ runtime/cpp/src/scalan/immutable_list.hpp
@@ -0,0 +1,131 @@
+#include <memory>
+#include <type_traits>
+#include <boost/iterator/iterator_facade.hpp>
+
+
+namespace scalan
+{
+    using std::shared_ptr;
+    namespace detail
+    {
+        template<class T>
+        struct immutable_list_node
+        {
+            typedef T value_type;
+
+            value_type val;
+            const shared_ptr<immutable_list_node<value_type>> next;
+
+            immutable_list_node() : val(value_type()) {}
+            immutable_list_node( const value_type &aV, const shared_ptr<immutable_list_node<value_type>> &aTail ): val(aV), next(aTail) {}
+        };
+
+        template<class P, class V>
+        class immutable_list_iterator_base: public boost::iterator_facade<immutable_list_iterator_base<P,V>, P, boost::forward_traversal_tag, typename std::add_lvalue_reference<V>::type>
+        {
+        public:
+            immutable_list_iterator_base(const shared_ptr<P>& aCur) : cur(aCur) {}
+
+        private:
+            shared_ptr<P> cur;
+
+            friend class boost::iterator_core_access;
+
+            void increment() { cur = cur->next; }
+
+            bool equal(const immutable_list_iterator_base& other) const
+            {
+                return this->cur == other.cur;
+            }
+
+            typename immutable_list_iterator_base::reference dereference() const { return cur->val; }
+        }; //class immutable_list_iterator_base
+    }
+
+    template<class T>
+    class immutable_list
+    {
+    private:
+        typedef detail::immutable_list_node<T> node_t;
+
+    public:
+        typedef T value_type;
+        typedef detail::immutable_list_iterator_base<node_t,T> iterator;
+        typedef detail::immutable_list_iterator_base<typename std::add_const<node_t>::type, typename std::add_const<T>::type> const_iterator;
+
+        immutable_list() : len(0) {}
+
+        immutable_list(const T& aV, const immutable_list<T>& aTail): len(aTail.len + 1), h(std::make_shared<node_t>(aV, aTail.h)) {}
+
+        immutable_list(const immutable_list<T>& aFirst, const immutable_list<T>& aSecond) {
+            auto l = loop( ++aFirst.begin(), aFirst.end(), aSecond );
+            this->len = l.len + 1;
+            this->h = std::make_shared<node_t>(*aFirst.begin(), l.h);
+        }
+
+        immutable_list(std::initializer_list<T> args)
+        {
+            auto l = loop( args.begin()+1, args.end(), immutable_list() );
+            this->len = l.len + 1;
+            this->h = std::make_shared<node_t>(*args.begin(), l.h);
+        }
+
+        bool empty() const
+        {
+            return !this->h; // implicit conversion via shared_ptr::operator bool()
+        }
+
+        T& front() const { return this->h->val; }
+
+        immutable_list<T> push_front(T aV) const
+        {
+            return immutable_list(aV, *this);
+        }
+
+        immutable_list<T> pop_front() const
+        {
+            return this->empty() ? immutable_list() : immutable_list(this->h->next);
+        }
+
+        iterator begin() const
+        {
+            return iterator(h);
+        }
+
+        iterator end() const
+        {
+            return iterator(immutable_list().h);
+        }
+
+        const_iterator cbegin() const
+        {
+            return const_iterator(h);
+        }
+
+        const_iterator cend() const
+        {
+            return const_iterator(immutable_list().h);
+        }
+
+        size_t length() const { return this->len; }
+
+    private:
+        size_t len;
+        shared_ptr<node_t> h;
+
+        explicit immutable_list(shared_ptr<node_t> aHead): h(aHead) {}
+
+        template<class Iterator>
+        immutable_list loop(Iterator cur, Iterator end, const immutable_list& tail)
+        {
+            if( cur != end )
+            {
+                auto val = *cur;
+                auto l = loop(++cur, end, tail);
+                return l.push_front(val);
+            }
+            else
+                return tail;
+        }
+    };
+} //namespace scalan
