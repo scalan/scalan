fc3906330265abdeb6a35cb7b3446685f5a67e08
Viktor Smirnov <Smirnov.Viktor@huawei.com>
2015-03-20 15:42:39 +0300

    Added check case for PairElem in fromJuggedArray() function.

--- community-edition/src/main/scala/scalan/collections/Collections.scala
+++ community-edition/src/main/scala/scalan/collections/Collections.scala
@@ -359,6 +359,14 @@ trait CollectionsDslSeq extends impl.CollectionsSeq { self: ScalanCommunityDslSe
         val segments = positions.zip(lens)
         val flat_arr = arr.asRep[Array[Array[a]]].flatMap{i => i}.toArray.asRep[Array[a]]
         mkNestedCollection(Collection.fromArray(flat_arr), Collection.fromArray(segments)).asRep[NColl[T]]
+      case pairE: PairElem[a, b] =>
+        implicit val ct = pairE.classTag
+        //val len = arr.length
+        val lens: Arr[Int] = arr.asRep[Array[Array[(a, b)]]].map(i => i.length)
+        val positions = lens.scan(0)((x,y) => x + y).dropRight(1).toArray
+        val segments = positions.zip(lens)
+        val flat_arr = arr.asRep[Array[Array[(a, b)]]].flatMap{i => i}.toArray.asRep[Array[(a, b)]]
+        mkNestedCollection(Collection.fromArray(flat_arr), Collection.fromArray(segments)).asRep[NColl[T]]
       case e => ???(s"Element is $e")
     }
   }
