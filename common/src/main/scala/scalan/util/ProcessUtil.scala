2eaf576768391430a335ea6f0bca4df52138cb8c
Alexey Romanov <alexey.romanov@huawei.com>
2015-03-27 15:37:59 +0300

    Fix process output not showing up in the exception

--- common/src/main/scala/scalan/util/ProcessUtil.scala
+++ common/src/main/scala/scalan/util/ProcessUtil.scala
@@ -4,29 +4,33 @@ import java.io.{InputStreamReader, BufferedReader, File}
 
 object ProcessUtil {
   def launch(workingDir: File, command: String*) {
-    val builder = new ProcessBuilder(command: _*)
     val absoluteWorkingDir = workingDir.getAbsoluteFile
-    builder.directory(absoluteWorkingDir)
-    builder.inheritIO()
+    val builder = new ProcessBuilder(command: _*).
+      directory(absoluteWorkingDir).
+      redirectErrorStream(true)
     val proc = builder.start()
     val exitCode = proc.waitFor()
+    val output = readOutput(proc)
     if (exitCode != 0) {
-      val stream = proc.getInputStream
-      try {
-        val sb = new StringBuilder()
-        val reader = new BufferedReader(new InputStreamReader(stream))
-        var line: String = reader.readLine()
-        while (line != null) {
-          sb.append(line).append("\n")
-          line = reader.readLine()
-        }
-        throw new RuntimeException(s"Executing '${command.mkString(" ")}' in directory $absoluteWorkingDir returned exit code $exitCode with following output:\n$sb")
-      } finally {
-        stream.close()
-      }
+      throw new RuntimeException(s"Executing '${command.mkString(" ")}' in directory $absoluteWorkingDir returned exit code $exitCode with following output:\n$output")
     } else {
-      // program executed successfully
+      Console.print(output)
     }
   }
 
+  def readOutput(process: Process) = {
+    val stream = process.getInputStream
+    try {
+      val sb = new StringBuilder()
+      val reader = new BufferedReader(new InputStreamReader(stream))
+      var line: String = reader.readLine()
+      while (line != null) {
+        sb.append(line).append("\n")
+        line = reader.readLine()
+      }
+      sb.result()
+    } finally {
+      stream.close()
+    }
+  }
 }
