51fd37b38f8e01a0b37f46d8f65ac93ea910e908
Adel Chepkunov <chepkunov.adel@huawei.com>
2015-04-08 14:58:28 +0300

    extract NscCompiler from cake

--- lms-backend/src/main/scala/scalan/compilation/lms/scalac/LmsCompilerScala.scala
+++ lms-backend/src/main/scala/scalan/compilation/lms/scalac/LmsCompilerScala.scala
@@ -9,7 +9,7 @@ import java.net.{URL, URLClassLoader}
 import scala.tools.nsc.reporters.StoreReporter
 import scala.tools.nsc.{Global, Settings}
 import scalan.compilation.language.MethodMappingDSL
-import scalan.compilation.lms.source2bin.{SbtConfig, SbtCompiler}
+import scalan.compilation.lms.source2bin.{Nsc, SbtConfig, SbtCompiler}
 import scalan.util.FileUtil
 import scalan.util.FileUtil.file
 
@@ -33,45 +33,16 @@ trait LmsCompilerScala extends LmsCompiler with CoreBridge with MethodMappingDSL
     val jarFile = file(executableDir.getAbsoluteFile, s"$functionName.jar")
     FileUtil.deleteIfExist(jarFile)
     val jarPath = jarFile.getAbsolutePath
-    val logFile = file(executableDir.getAbsoluteFile, s"$functionName.log")
-    FileUtil.deleteIfExist(logFile)
     val mainClass : Option[String] = compilerConfig.sbt.mainPack match {
       case Some(mainPack) => Some(mainPack + "." +  functionName)
       case _ =>  None
     }
     val dependencies:Array[String] = methodReplaceConf.flatMap(conf => conf.dependencies).toArray
     val output: Option[Array[String]] = compilerConfig.scalaVersion match {
-      case Some(scalaVersion) => Some(SbtCompiler.sbtCompile(sourcesDir, executableDir, functionName, compilerConfig, dependencies, sourceFile, jarPath))
+      case Some(scalaVersion) =>
+        Some(SbtCompiler.sbtCompile(sourcesDir, executableDir, functionName, compilerConfig, dependencies, sourceFile, jarPath))
       case None =>
-        val settings = new Settings
-        settings.usejavacp.value = true
-        // necessary to lauch compiler
-        // see http://stackoverflow.com/questions/27934282/object-scala-in-compiler-mirror-not-found-running-scala-compiler-programatical
-        settings.embeddedDefaults[LmsCompilerScala]
-        val compilerOptions = "-d" :: jarPath :: compilerConfig.extraCompilerOptions.toList
-        settings.processArguments(compilerOptions, false)
-        val reporter = new StoreReporter
-        val compiler: Global = new Global(settings, reporter)
-        val run = new compiler.Run
-        run.compile(List(sourceFile.getAbsolutePath))
-
-        import java.io.PrintWriter
-        val S = new PrintWriter(logFile)
-        S.println(settings)
-        S.println(s"${settings.classpath}\n")
-        for(row <- reporter.infos) {
-          S.println(s"${row.severity}: ${row.msg}")
-          S.println(s"|${row.pos.source.path}:${row.pos.safeLine}")
-          S.println(s"|${row.pos.lineContent}")
-          S.println("|"+" "*(row.pos.column-1) + "^")
-        }
-        S.println(s"class $functionName compiled with ${reporter.ERROR.count} errors and ${reporter.WARNING.count} warnings")
-        S.close()
-
-        reporter.ERROR.count match {
-          case 0 => //println(s"class $functionName compiled with ${reporter.WARNING.count} warnings")
-          case _ => throw new Exception(s"class $functionName compiled with ${reporter.ERROR.count} errors and ${reporter.WARNING.count} warnings, see ${logFile.getAbsolutePath} for details")
-        }
+        Nsc.compile(executableDir, functionName, compilerConfig.extraCompilerOptions.toList, sourceFile, jarPath)
         None
     }
     CustomCompilerOutput(jarFile.toURI.toURL, mainClass, output)
