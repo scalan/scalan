6e17573b19e57aed5b8128fbdebdaf16cad40956
Viktor Smirnov <Smirnov.Viktor@huawei.com>
2015-04-13 09:43:54 +0300

    Added attach method to Segments Domain Type.

--- core/src/test/scala/scalan/common/Segments.scala
+++ core/src/test/scala/scalan/common/Segments.scala
@@ -11,18 +11,42 @@ trait Segments { self: SegmentsDsl =>
     def length: Rep[Int]
     def end: Rep[Int]
     def shift(ofs: Rep[Int]): Rep[Segment]
+    def attach(seg: Rep[Segment]): Rep[Segment]
   }
   trait SegmentCompanion
 
   abstract class Interval(val start: Rep[Int], val end: Rep[Int]) extends Segment {
     def length = end - start
     def shift(ofs: Rep[Int]) = Interval(start + ofs, end + ofs)
+    def attach(seg: Rep[Segment]): Rep[Segment] = matchSegment(seg) {
+      ivl => 
+        val startMin = Math.min(start, seg.start)
+        val startMax = Math.min(start, seg.start)
+        val endMin = Math.min(start, seg.start)
+        val endMax = Math.min(start, seg.start)
+        ???
+    } {
+      slc => ???
+    } {
+      cnt => ???
+    } {
+      abs => ???
+    }
   }
   trait IntervalCompanion
 
   abstract class Slice(val start: Rep[Int], val length: Rep[Int]) extends Segment {
     def end = start + length
     def shift(ofs: Rep[Int]) = Slice(start + ofs, length)
+    def attach(seg: Rep[Segment]): Rep[Segment] = matchSegment(seg) {
+      ivl => ???
+    } {
+      slc => ???
+    } {
+      cnt => ???
+    } {
+      abs => ???
+    }
   }
   trait SliceCompanion
 
@@ -31,6 +55,15 @@ trait Segments { self: SegmentsDsl =>
     def end = center + radius
     def length = radius * 2
     def shift(ofs: Rep[Int]) = Centered(center + ofs, radius)
+    def attach(seg: Rep[Segment]): Rep[Segment] = matchSegment(seg) {
+      ivl => ???
+    } {
+      slc => ???
+    } {
+      cnt => ???
+    } {
+      abs => ???
+    }
   }
   trait CenteredCompanion
 }
