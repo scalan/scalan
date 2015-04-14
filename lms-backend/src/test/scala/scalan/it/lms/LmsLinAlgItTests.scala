0cec102dfc65b7cd543fef2340597684c67c3058
Alexey Romanov <alexey.romanov@huawei.com>
2015-04-14 11:34:14 +0300

    Mark a test as pending

--- lms-backend/src/test/scala/scalan/it/lms/LmsLinAlgItTests.scala
+++ lms-backend/src/test/scala/scalan/it/lms/LmsLinAlgItTests.scala
@@ -180,15 +180,16 @@ class AbstractElemItTests extends LmsLinAlgItTests {
   }
 
   test("elems divergence in if_then_else branches") {
+    // Different branches of if_then_else operator unexpectedly produce different elems.
+    // This causes Sums and SumViews to appear.
+    // Produced code could not be executed. The test only build final graph.
+    // Bad behavior remains (with a different graph) if DenseVector.+^ is changed to return DenseVector
+    pending
     val matrix = Array(Array(0, 5), Array(1, 3), Array(1, 4))
     val vector = Array(1,2)
 
     val progStaged = new ProgExp
     val in = Tuple(matrix, vector)
-    // Different branches of if_then_else operator unexpectedly produce different elems.
-    // This causes Sums and SumViews to appear.
-    // Produced code could not be executed. The test only build final graph.
-    // Bad behavior remains (with a different graph) if DenseVector.+^ is changed to return DenseVector
     progStaged.buildGraph(file(prefix, "simpleSum"), "simpleSum", progStaged.funSimpleSum, graphVizConfig)(progStaged.defaultCompilerConfig)
   }
 
