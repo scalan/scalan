47336d92a9c4505e25997996b76c99aac2cf0837
Viktor Smirnov <Smirnov.Viktor@huawei.com>
2015-04-14 13:32:44 +0300

    Added MetaTests DSL.

---
+++ core/src/test/scala/scalan/common/MetaTests.scala
@@ -0,0 +1,51 @@
+package scalan.common
+
+/**
+ * Created by Victor Smirnov on 4/14/15.
+ */
+
+trait MetaTests { self: MetaTestsDsl =>
+
+  type RMetaTest[T] = Rep[MetaTest[T]]
+  trait MetaTest[T] extends Reifiable[MetaTest[T]] { self =>
+
+    def test: RMetaTest[T]
+    def give: Rep[T]
+    def size: Rep[Int]
+    implicit def elem: Elem[T]
+  }
+  trait MetaTestCompanion
+
+  abstract class MT0(val size: Rep[Int]) extends MetaTest[Unit] {
+
+    def test: RMetaTest[Unit] = ???
+    def give: Rep[Unit] = ???
+    def elem = UnitElement
+  }
+  trait MT0Companion
+
+  abstract class MT1[T](val data: Rep[T], val size: Rep[Int])(implicit val elem: Elem[T]) extends MetaTest[T] {
+
+    def test: RMetaTest[T] = ???
+    def give: Rep[T] = ???
+  }
+  trait MT1Companion
+
+  abstract class MT2[T, R](val indices: Rep[T], val values: Rep[R], val size: Rep[Int])
+                          (implicit val eT: Elem[T], implicit val eR: Elem[R]) extends MetaTest[(T, R)] {
+
+    def test: RMetaTest[(T, R)] = ???
+    def give: Rep[(T, R)] = ???
+    lazy val elem = element[(T, R)]
+  }
+  trait MT2Companion
+}
+
+trait MetaTestsDsl extends impl.MetaTestsAbs {
+}
+
+trait MetaTestsDslSeq extends impl.MetaTestsSeq {
+}
+
+trait MetaTestsDslExp extends impl.MetaTestsExp {
+}
