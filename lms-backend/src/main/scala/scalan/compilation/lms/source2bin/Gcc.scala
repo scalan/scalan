1d8bb1414489a6a1e5db4e06af4e68d99ceb9b62
adel <chepkunov.adel@huawei.com>
2015-05-28 11:34:51 +0300

    two codegen: jet path to java from environment

--- lms-backend/src/main/scala/scalan/compilation/lms/source2bin/Gcc.scala
+++ lms-backend/src/main/scala/scalan/compilation/lms/source2bin/Gcc.scala
@@ -11,17 +11,19 @@ object Gcc {
   def compile(targetDir: String, sourceDir: File, sourceFile: File, libName: String): Array[String] = {  //extraCompilerOptions: List[String],
     val sourceName = sourceFile.getName
     val targetFile = targetDir+fileSeparator+libFileName(libName)
-    val include = "-I/usr/include -I/usr/lib/jvm/java-7-oracle/include -I/usr/lib/jvm/java-7-oracle/include/linux -I"+includeRuntimeDir
+    val include = s"-I/usr/include $includeJavaFlag $includeRuntimeDirFlag"
     val command = s"g++ $sourceName $include -fPIC -shared -pthread -std=c++0x -o $targetFile".split(" ").toSeq
     println("command: " + command.mkString(" "))
     ProcessUtil.launch(sourceDir, command: _*)
 
   }
 
-  def includeRuntimeDir: String = {
-    System.getProperty("user.dir") + fileSeparator + scalan.Base.config.getProperty("runtime.include")
+  def includeRuntimeDirFlag: String = {
+    "-I"+System.getProperty("user.dir") + fileSeparator + scalan.Base.config.getProperty("runtime.include")
   }
 
+  val javaHome = System.getProperty("java.home")+"/.."
+  val includeJavaFlag = s"-I$javaHome/include -I$javaHome/include/linux"
   val fileSeparator = System.getProperty("file.separator")
   val osName = System.getProperty("os.name")
   def libFileName(libName:String) = {
