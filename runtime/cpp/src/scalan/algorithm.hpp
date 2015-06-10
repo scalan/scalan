043ee3cbd80e71d2d401dfe7f556e0af58faffdd
Zotov Yuriy <zotov.yuriy@huawei.com>
2015-06-10 15:38:58 +0300

    comment in algorithm.hpp

--- runtime/cpp/src/scalan/algorithm.hpp
+++ runtime/cpp/src/scalan/algorithm.hpp
@@ -4,7 +4,16 @@
 #include <algorithm>
 
 namespace scalan {
-
+    /***
+     * Returns:
+     * index of the search key, if it is contained in the array;
+     * otherwise, (-(insertion point) - 1).
+     * The insertion point is defined as the point at which the key would be inserted
+     * into the array: the index of the first element greater than the key,
+     * or a.length if all elements in the array are less than the specified key.
+     * Note that this guarantees that the return value will be >= 0 if and only if
+     * the key is found.
+    */
     template<class RandomIt, class T>
     int32_t binary_search(RandomIt begin, RandomIt end, const T& val)
     {
