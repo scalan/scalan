49ac4b2e1a91c5c365ef06ca6e4e75d504d75889
Zotov Yuriy <zotov.yuriy@huawei.com>
2015-06-16 09:20:41 +0300

    redundant tests removed

--- lms-backend/src/it/scala/scalan/it/lms/LmsJNIExtractorItTests.scala
+++ lms-backend/src/it/scala/scalan/it/lms/LmsJNIExtractorItTests.scala
@@ -135,49 +135,4 @@ class LmsJNIExtractorItTests extends BaseItTests {
     ctx.generate("sdmvm", ctx.sdmvm)
 //    ctx.generate("packPair", ctx.packPair)
   }
-
-  test("simpleBuildExecutable") { //TODO: automate executable building
-    pending
-    val ctx = new ScalanCtxExp with ProgExp with FirstProg {
-      override def subfolder: String = super.subfolder + "-build"
-
-      def test() = {
-      }
-
-      def buildExecutable[A,B](name: String, f: Exp[A => B]): Unit = {
-        val dir = new File(prefix, subfolder)
-        buildExecutable(dir, dir, name, f, GraphVizConfig.default)
-      }
-    }
-
-    ctx.buildExecutable("extractDouble", ctx.extractDouble)
-  }
-
-  test("simpleExecute") { //TODO: automate execution
-    pending
-//    val ctx = new ScalanCtxExp with ProgExp with FirstProg {
-//      override def subfolder: String = "release"
-//
-//      def test() = {
-//      }
-//
-//      def execute(): Unit = {
-//        val dir = new File(prefix, subfolder)
-////        System.setProperty("java.library.path", dir.getAbsolutePath) //TODO: is there a way to set path dynamically?
-//        println(s"java.library.path=${System.getProperty("java.library.path")}")
-//        System.loadLibrary("jniExtractor")
-//
-////        val m = scala.Array(scala.Array(34.0, 89.0),scala.Array(12.0, 43.0))
-//        val m = scala.Array( (scala.Array(0,1), (scala.Array(34.0, 88.0), 2))
-//                           , (scala.Array(0,1), (scala.Array(12.0, 43.0), 2)) )
-//        val v = scala.Array(77.0, 12.0)
-//
-//        val res = (new MVMs).extractorTest( (m,v) )
-//        println(res.mkString("[",", ","]"))
-//        //assert(res sameElements (v map {a => k*a}) )
-//      }
-//    }
-//
-//    ctx.execute
-  }
 }
