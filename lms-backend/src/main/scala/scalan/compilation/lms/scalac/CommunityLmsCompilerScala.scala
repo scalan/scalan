585ca655345f28f662d6f946fec64b46dece9079
Zotov Yuriy <zotov.yuriy@huawei.com>
2015-07-03 16:33:55 +0300

    move CollectionsMethodMapping to separate trait anf mix it into ScalaCompiler's trait

--- lms-backend/src/main/scala/scalan/compilation/lms/scalac/CommunityLmsCompilerScala.scala
+++ lms-backend/src/main/scala/scalan/compilation/lms/scalac/CommunityLmsCompilerScala.scala
@@ -3,42 +3,12 @@ package scalan.compilation.lms.scalac
 import java.io.File
 
 import scala.collection.Seq
-import scala.reflect.runtime.universe._
-import scalan.ScalanCommunityDslExp
+import scalan.{ScalanCommunityScalaCompiler, ScalanCommunityDslExp}
 import scalan.compilation.GraphVizConfig
 import scalan.compilation.lms.CommunityBridgeScala
 
-trait CommunityLmsCompilerScala extends LmsCompilerScala with CommunityBridgeScala with ScalanCommunityDslExp { self: ScalanCommunityDslExp =>
+trait CommunityLmsCompilerScala extends LmsCompilerScala with CommunityBridgeScala with ScalanCommunityScalaCompiler { self: ScalanCommunityDslExp =>
 
-  import scala.language.reflectiveCalls
-
-  new ScalaMappingDSL with MappingTags {
-
-    val scalan_collections_SSeq = {
-      val sseqClass = findDefinition(SSeq) match {
-        case Some(TableEntry(sym, rhs)) =>
-          rhs.getClass
-      }
-
-      new ClassType(Symbol(sseqClass.getName)) {
-        val apply = Method('apply, typeOf[Seq[_]], MethodArg(typeOf[Array[_]]))
-        val single = Method('single, typeOf[Seq[_]])
-        val empty = Method('empty, typeOf[Seq[_]])
-      }
-    }
-
-    val scala_collection_Seq = new ScalaLib() {
-      val arrayToList = ScalaFunc(Symbol("(new AnyRef {def apply[T](arr: Array[T]):List[T] = arr.toList})"))(false)
-      val single = ScalaFunc(Symbol("scala.collection.Seq"))(false)
-      val empty = ScalaFunc(Symbol("Seq.empty"))(false)
-    }
-
-    val mapping = new ScalaMapping {
-      val functionMap = Map( scalan_collections_SSeq.apply -> scala_collection_Seq.arrayToList
-                           , scalan_collections_SSeq.empty -> scala_collection_Seq.empty
-                           , scalan_collections_SSeq.single -> scala_collection_Seq.single)
-    }
-  }
 
   override protected def doBuildExecutable[A, B](sourcesDir: File, executableDir: File, functionName: String, graph: PGraph, graphVizConfig: GraphVizConfig)
                                                 (compilerConfig: CompilerConfig, eInput: Elem[A], eOutput: Elem[B]) = {
