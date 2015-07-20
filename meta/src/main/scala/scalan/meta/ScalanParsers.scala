c41aaf287c409646e1ec592d2dfafe91b7ffabc2
Maxim Gekk <gekk.maxim@huawei.com>
2015-07-20 10:51:48 +0300

    Handling of type tree when type args are parsed.

--- meta/src/main/scala/scalan/meta/ScalanParsers.scala
+++ meta/src/main/scala/scalan/meta/ScalanParsers.scala
@@ -157,7 +157,10 @@ trait ScalanParsers {
             None
           else
             optTpeExpr(high)
-        case tt: TypeTree => Some(parseType(tt.tpe)) // TODO: Extract high bound
+        case tt: TypeTree => parseType(tt.tpe) match {
+          case STpeTypeBounds(_, hi) if hi.toString == "scala.Any" => Some(hi)
+          case _ => ???(tdTree)
+        }
         case _ => ???(tdTree)
       }
       val contextBounds = evidenceTypes.collect {
@@ -496,9 +499,9 @@ trait ScalanParsers {
   }
 
   def parseTypeRef(tref: TypeRef): STpeExpr = {
-    val pre = tref.prefixString
+    val name = tref.sym.fullNameString
     val args = tref.args map parseType
 
-    STraitCall(pre, args)
+    STraitCall(name, args)
   }
 }
