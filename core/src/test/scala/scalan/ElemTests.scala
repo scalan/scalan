2b93f87d7eb46d501a1b87438eb813c73c376e2e
Alexey Romanov <alexey.romanov@huawei.com>
2015-07-31 09:37:38 +0300

    Add tests for Element and Container equality

---
+++ core/src/test/scala/scalan/ElemTests.scala
@@ -0,0 +1,61 @@
+package scalan
+
+import scalan.common.{KindsDslExp, KindsExamples}
+
+abstract class AbstractElemTests extends BaseTests {
+  class Ctx extends ScalanCtxExp {
+    def elementsShouldBeEqual[A: Elem, B: Elem] =
+      assert(element[A] == element[B])
+    def elementsShouldNotBeEqual[A: Elem, B: Elem] =
+      assert(element[A] != element[B])
+
+    def containersShouldBeEqual[A[_]: Cont, B[_]: Cont] =
+      assert(container[A] == container[B])
+    def containersShouldNotBeEqual[A[_]: Cont, B[_]: Cont] =
+      assert(container[A] != container[B])
+  }
+}
+
+class ElemTests extends AbstractElemTests {
+  class Ctx extends super.Ctx with KindsExamples with KindsDslExp
+
+  test("Element equality works as expected") {
+    val ctx = new Ctx
+    import ctx._
+
+    elementsShouldBeEqual[Int, Int]
+    elementsShouldBeEqual[Double, Double]
+    elementsShouldNotBeEqual[Int, Double]
+
+    elementsShouldBeEqual[Array[Int], Array[Int]]
+    elementsShouldNotBeEqual[Array[Int], Array[Double]]
+    elementsShouldNotBeEqual[Array[Int], List[Int]]
+
+    elementsShouldBeEqual[Double => Int, Double => Int]
+    elementsShouldNotBeEqual[Int => Double, Double => Int]
+
+    elementsShouldBeEqual[(Int, Double), (Int, Double)]
+    elementsShouldNotBeEqual[(Int, Double), (Double, Double)]
+
+    elementsShouldBeEqual[Int | Int, Int | Int]
+    elementsShouldNotBeEqual[Int | Int, Int]
+
+    elementsShouldBeEqual[AString, AString]
+    elementsShouldNotBeEqual[String, AString]
+    elementsShouldBeEqual[CString, CString]
+    elementsShouldNotBeEqual[AString, CString]
+    elementsShouldBeEqual[SThrowable, SThrowable]
+    elementsShouldBeEqual[SException, SException]
+    elementsShouldBeEqual[Thunk[Int], Thunk[Int]]
+  }
+
+  test("Container equality works as expected") {
+    val ctx = new Ctx
+    import ctx._
+
+    containersShouldBeEqual[ArrayBuffer, ArrayBuffer]
+    containersShouldBeEqual[Array, Array]
+    containersShouldBeEqual[Id, Id]
+    containersShouldNotBeEqual[Array, ArrayBuffer]
+  }
+}
---
+++ library/src/test/scala/scalan/effects/HigherKindContainerTests.scala
@@ -0,0 +1,26 @@
+package scalan.effects
+
+import scalan.AbstractElemTests
+import scalan.monads.MonadsDslExp
+
+class HigherKindContainerTests extends AbstractElemTests {
+  class Ctx extends super.Ctx with MonadsDslExp
+
+  test("Equality works as expected on containers with higher-kind type variables") {
+    // this doesn't currently work
+    pending
+
+    val ctx = new Ctx
+    import ctx._
+
+    type CP_A_AB[A] = Coproduct[Array, ArrayBuffer, A]
+    type CP_AB_A[A] = Coproduct[ArrayBuffer, Array, A]
+
+    containersShouldBeEqual[CP_A_AB, CP_A_AB]
+    containersShouldNotBeEqual[CP_A_AB, CP_AB_A]
+
+    type SF_I[A] = StateF[Int, A]
+
+    containersShouldBeEqual[SF_I, SF_I]
+  }
+}
