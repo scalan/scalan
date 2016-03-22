e3d70663456718f1c54447422ee1771586ea60dd
Alexey Romanov <alexey.romanov@huawei.com>
2016-03-22 17:22:32 +0300

    Emit boolean ops in C++ backend

--- lms-backend/core/src/main/scala/scalan/compilation/lms/cxx/sharedptr/CoreCxxShptrLmsBackend.scala
+++ lms-backend/core/src/main/scala/scalan/compilation/lms/cxx/sharedptr/CoreCxxShptrLmsBackend.scala
@@ -14,6 +14,7 @@ class CxxCoreCodegen[BackendCake <: LmsBackendFacade with JNILmsOpsExp](backend:
   with CLikeGenEqual
   with CLikeGenPrimitiveOps
   with CLikeGenMathOps
+  with CLikeGenBooleanOps
   with CxxShptrGenStruct
   with CxxShptrGenFatArrayLoopsFusionOpt
   with LoopFusionOpt
